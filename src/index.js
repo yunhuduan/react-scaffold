12d6b25369e4b5e5ced4baf95b05953c758bd23e
yunhuduan <duanyunhu@sina.cn>
2016-10-16 12:02:27 +0800

    all done start to dev

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React, {Component}    from "react";
-import ReactDOM              from "react-dom";
-import World              from "./components/World";
-console.log(new Date().getTime())
-ReactDOM.render(
-  <World/>,
-  document.getElementById('app')
-);
